c3c703eb5b5f607731e674fa30724a17adb3dbd4
Máximo Cuadros Ortiz <mcuadros@gmail.com>
2013-04-20 17:58:45 +0200

    pretty help

--- src/github.com/mcuadros/console/help.go
+++ src/github.com/mcuadros/console/help.go
@@ -2,24 +2,24 @@ package console
 
 func NewHelpCommand() *Command {
 	cmd := NewCommand("help")
+	cmd.SetDescription("Displays help for a given command")
+	cmd.SetHelp("The help command displays help for a given command")
 
-	var opts struct {
+	var options struct {
 		Command string `short:"c" long:"command" description:"Command name" required:"true"`
 	}
+	cmd.SetOptions(&options)
 
-	var HelpHandler = func(app *Application) {
+	var handler = func(app *Application) {
 		commands := app.GetCommands()
-		commands[opts.Command].PrintHelp(app.GetOutput())
+		commands[options.Command].PrintHelp(app.GetOutput())
 
 		app.GetOutput().Write("\n@{y}Available commands:\n", 10)
 		for name, command := range commands {
 			app.GetOutput().Write("  @{g}%-20s@{!}\t%s\n", 10, name, command.GetDescription())
 		}
 	}
+	cmd.SetHandler(handler)
 
-	cmd.SetDescription("Displays help for a given command")
-	cmd.SetHelp("The help command displays help for a given command")
-	cmd.SetHandler(HelpHandler)
-	cmd.SetOptions(&opts)
 	return cmd
 }
